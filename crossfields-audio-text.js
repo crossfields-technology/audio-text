e9181a026137dce9a5e9ed9b5d7fa12344350310
Nathan Collins <nathanc@crossfields.com.au>
2017-01-05 01:16:09 +1000

    Audio text 2 speech generated

--- crossfields-audio-text.js
+++ crossfields-audio-text.js
@@ -6,7 +6,38 @@ Polymer({
     text: {
       type: String,
       observer: 'playText'
+    },
+
+    apiKey: {
+      type: String,
+      value: '6a663a11b30e46988b3e49baad605e9f'
+    },
+
+    language: {
+      type: String,
+      value: 'en-au'
+    },
+
+    format: {
+      type: String,
+      value: 'mp3'
     }
   },
+
+  playText: function() {
+    try {
+      VoiceRSS.speech({
+        key: this.apiKey,
+        src: this.text,
+        hl: this.language,
+        r: 0,
+        c: this.format,
+        f: '44khz_16bit_stereo',
+        ssml: false
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
 });
