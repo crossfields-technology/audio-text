698f132de8b6b44b28335785c24200943e1a7f29
Nathan Collins <nathanc@crossfields.com.au>
2017-01-05 21:46:51 +1000

    Added more documentation

--- crossfields-audio-text.js
+++ crossfields-audio-text.js
@@ -3,31 +3,39 @@ Polymer({
   is: 'crossfields-audio-text',
 
   properties: {
+    /** The textual content for converting to speech (length limited by 100KB) */
     text: {
       type: String,
       observer: 'playText'
     },
 
+    /** The api key from voiceRSS */
     apiKey: {
-      type: String,
-      value: '6a663a11b30e46988b3e49baad605e9f'
+      type: String
     },
 
+    /** The language spoken from the automated audio. */
     language: {
       type: String,
       value: 'en-au'
     },
 
+    /** The speech audio codec. Default value: mp3 */
     codec: {
       type: String,
       value: "mp3"
     },
 
+    /** The speech audio formats. */
     format: {
       type: String,
       value: '44khz_16bit_stereo'
     },
 
+    /** The speech rate (speed).
+      *  Allows value from -10 (slowest speed) up to 10 (fastest speed).
+      *  Default value 0 (normal speed).
+      */
     rate: {
       type: Number,
       value: 0
